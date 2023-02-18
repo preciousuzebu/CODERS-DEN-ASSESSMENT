00bd1191b5294e965a8ffc18e836e6f7033e0670
preciousuzebu <uzebuesohe@gmail.com>
2023-02-18 09:39:18 +0100

    day1 and day2

---
+++ day1.js
@@ -0,0 +1,79 @@
+//QUESTION 1 -------Write a single line comment which says, comments can make code readable
+//comments can make code readable
+
+
+//Question 2---------Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript
+
+
+// Question 3---------Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*comments can make code readable, easy to reuse and informative*/
+
+
+//Question 4------Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+
+   /* let color = "Red";   
+      let isActive = false;    
+      let x;  
+      let y = null;  */     
+
+
+  
+
+
+// Question 5-------Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
+
+//console.log(typeof Red)   string
+//console.log(typeof  false)  booleen
+//console.log(typeof  x)    Undefined
+//console.log(typeof  null)    Null
+
+
+
+// Question 6-------Declare four variables without assigning values
+
+/*let name;
+  const age;
+  let country;
+  let lastName;
+*/
+
+// Question  7--------Declare four variables with assigned values
+
+/*
+  let name ="Precious";
+  const age= 50 ; 
+  let country="Nigeria";
+  let lastName= "John";
+*/ 
+
+
+//Question 8-------Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+ /* let firstName ='Precious' 
+    lastName ='John' 
+    maritalStatus ='Single' 
+    Country = 'Nigeria'
+    Age = 28
+    console.log(firstName, lastName, maritalStatus, Country, Age)*/
+
+
+
+
+//Question 9--------Declare variables to store your first name, last name, marital status, country and age in a single line
+
+/*
+    let firstName ='Precious' , lastName ='John' , maritalStatus ='Single' , Country ='Nigeria', Age ='28'
+    console.log(firstName, lastName, maritalStatus, Country, Age)
+ */
+
+//Declare two variables myAge and yourAge and assign them initial values and log to the browser console.
+
+/*
+  const myAge = 28
+  const yourAge = 30
+
+  console.log(`I am ${myAge} years old.`)
+  console.log(`You are ${yourAage} years old.`);
+ */
---
+++ day2.js
@@ -0,0 +1,148 @@
+//Question   1--------Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+/* let challenge = "30 Days of Javascript"
+
+
+//Question   2--------Print the string on the browser console using console.log()
+let challenge = "30 Days of Javascript"
+console.log(challenge)
+
+
+//Question   3--------Print the length of the string on the browser console using console.log()
+let challenge = "30 Days of Javascript"
+console.log(challenge.length)
+
+
+//Question   4--------Change all the string characters to capital letters using toUpperCase() method 
+let challenge = "30 Days of Javascript"
+console.log(challenge.toUpperCase())
+
+
+//Question   5--------Change all the string characters to lowercase letters using toLowerCase() method
+let challenge = "30 Days of Javascript"
+console.log(challenge.toLowerCase()) 
+
+
+//Question   6--------Cut (slice) out the first word of the string using substr() or substring() method
+let challenge = "30 Days of Javascript"
+console.log(challenge.substring(0,2)) 
+
+
+
+//Question   7--------Slice out the phrase Days Of JavaScript from 30 Days Of JavaScript.
+let challenge = "30 Days of Javascript"
+console.log(challenge.substr(2,16))
+
+
+
+//Question   8--------Check if the string contains a word Script using includes() method
+let challenge = "30 Days of Javascript"
+console.log(challenge.includes('Script'))
+
+
+
+//Question   9--------Split the string into an array using split() method
+let challenge = "30 Days of Javascript"
+console.log(challenge.split())
+
+
+
+//Question   10--------Split the string 30 Days Of JavaScript at the space using split() method
+let challenge = "30 Days of Javascript"
+console.log(challenge.split(' '))
+
+
+
+//Question   11--------'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' split the string at the comma and change it to an array.
+let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'
+console.log(companies.split(','))
+
+
+
+//Question   12--------Change 30 Days Of JavaScript to 30 Days Of Python using replace() method.
+let challenge = "30 Days of Javascript"
+console.log(challenge.replace('Javascript','Python'))
+
+
+
+//Question   13--------What is character at index 15 in '30 Days Of JavaScript' string? Use charAt() method.
+let challenge = "30 Days of Javascript"
+console.log(challenge.charAt(15))  --------i
+
+
+
+//Question   14--------What is the character code of J in '30 Days Of JavaScript' string using charCodeAt()
+let challenge = "30 Days of Javascript"
+console.log(challenge.charCodeAt(8))
+
+
+
+//Question   15--------Use indexOf to determine the position of the first occurrence of a in 30 Days Of JavaScript
+let challenge = "30 Days of Javascript"
+console.log(challenge.indexOf('a'))----------4
+
+
+//Question   16--------Use lastIndexOf to determine the position of the last occurrence of a in 30 Days Of JavaScript.
+let challenge = "30 Days of Javascript"
+console.log(challenge.lastIndex('a'))
+
+
+//Question   17--------Use indexOf to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let string = 'You cannot end a sentence with because because because is a conjunction'
+console.log(string.indexOf('because'))
+
+
+
+//Question   18--------Use lastIndexOf to find the position of the last occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let string = 'You cannot end a sentence with because because because is a conjunction'
+console.log(string.lastIndex('because'))
+
+
+
+
+//Question   19--------Use search to find the position of the first occurrence of the word because in the following sentence:'You cannot end a sentence with because because because is a conjunction'
+let string = 'You cannot end a sentence with because because because is a conjunction'
+console.log(string.search('because'))
+
+
+
+
+//Question   20--------Use trim() to remove any trailing whitespace at the beginning and the end of a string.E.g ' 30 Days Of JavaScript '.
+let challenge = "30 Days of Javascript"
+console.log(chanllege.trim(''))
+
+
+
+
+//Question   21--------Use startsWith() method with the string 30 Days Of JavaScript and make the result true
+let challenge = "30 Days of Javascript"
+console.log(challenge.startWith('30 Days Of Javascript'))    ------------true
+
+
+
+
+
+//Question   22--------Use endsWith() method with the string 30 Days Of JavaScript and make the result true
+let challenge = "30 Days of Javascript"
+console.log(challenge.endsWith('30 Days of Javascript')) ------------true
+
+
+
+
+//Question   23--------Use match() method to find all the a’s in 30 Days Of JavaScript
+let challenge = /30 Days of Javascript/gi
+console.log(challenge.match('a'))
+
+
+
+//Question   24--------Use concat() and merge '30 Days of' and 'JavaScript' to a single string, '30 Days Of JavaScript'
+let string = "30 Days of"
+console.log(string.concat("Javascript"))
+
+
+
+//Question   25--------Use repeat() method to print 30 Days Of JavaScript 2 times
+let challenge = "30 Days of Javascript"
+console.log(challenge.repeat(2))
+*/
+
+
